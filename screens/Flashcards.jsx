a043b993b6ac4de3d27c779b8f2c43941a428c32
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-12-23 21:14:27 +0800

    development: Added modal to Flashcards.jsx

--- screens/Flashcards.jsx
+++ screens/Flashcards.jsx
@@ -5,10 +5,16 @@ import {
 	TextInput,
 	TouchableOpacity,
 	View,
+	Modal,
+	TouchableWithoutFeedback,
+	Dimensions,
 } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
+
+const PRIMARY_COLOR = "#ab39c6";
+const CURRENT_HEIGHT = Dimensions.get("window").height;
 
 const FlashcardItem = ({ category, description, no_of_items }) => {
 	return (
@@ -29,7 +35,8 @@ const Flashcards = () => {
 	const [flashcards, setFlashcards] = useState([
 		{
 			category: "Marvel Superheroes",
-			description: "A questions about marvel superheroes",
+			description:
+				"A questions about marvel superheroes. This includes the power, the origin, and the land of the heroes",
 			no_of_items: 5,
 		},
 		{
@@ -113,6 +120,7 @@ const Flashcards = () => {
 			no_of_items: 5,
 		},
 	]);
+	const [modalVisible, setModalVisible] = useState(false);
 
 	return (
 		<SafeAreaProvider>
@@ -129,17 +137,64 @@ const Flashcards = () => {
 					contentContainerStyle={{ paddingBottom: 20 }}
 				/>
 
-				<TouchableOpacity style={styles.fixedButton}>
+				<TouchableOpacity
+					style={styles.fixedButton}
+					onPress={() => {
+						setModalVisible(true);
+					}}
+				>
 					<Text
 						style={{
 							textAlign: "center",
 							fontFamily: "PoppinsMedium",
 							fontSize: 32,
+							color: "white",
 						}}
 					>
 						+
 					</Text>
 				</TouchableOpacity>
+
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						setModalVisible(false);
+					}}
+				>
+					<View
+						style={styles.modalBackdrop}
+						onStartShouldSetResponder={(event) => setModalVisible(false)}
+					>
+						<TouchableWithoutFeedback
+							onPress={(event) => event.stopPropagation()}
+						>
+							<View style={styles.modalContent}>
+								<Text style={styles.modalContentTitle}>New Category</Text>
+								<View style={styles.formSection}>
+									<Text style={styles.formLabel}>Category</Text>
+									<TextInput
+										style={styles.formTextInput}
+										placeholder="Ex. National Heroes"
+									/>
+								</View>
+								<View style={styles.formSection}>
+									<Text style={styles.formLabel}>Description</Text>
+									<TextInput
+										style={styles.formTextInput}
+										placeholder="Ex. National Heroes"
+									/>
+								</View>
+								<TouchableOpacity style={styles.addCategoryButton}>
+									<View>
+										<Text style={styles.addCategoryButtonText}>Add</Text>
+									</View>
+								</TouchableOpacity>
+							</View>
+						</TouchableWithoutFeedback>
+					</View>
+				</Modal>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	);
@@ -148,6 +203,57 @@ const Flashcards = () => {
 export default Flashcards;
 
 const styles = StyleSheet.create({
+	modalBackdrop: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "flex-end",
+	},
+	modalContent: {
+		backgroundColor: "white",
+		width: "100%",
+		height: CURRENT_HEIGHT * 0.5,
+		shadowColor: "#000", // Shadow color
+		shadowOffset: { width: 0, height: 25 },
+		shadowOpacity: 1, // Shadow opacity
+		shadowRadius: 252, // Shadow blur radius
+		elevation: 5,
+		borderRadius: 15,
+		paddingVertical: 25,
+		paddingHorizontal: 15,
+		gap: 15,
+	},
+	modalContentTitle: {
+		fontFamily: "PoppinsBold",
+		fontSize: 20,
+		color: PRIMARY_COLOR,
+	},
+	formSection: {
+		marginTop: 5,
+	},
+	formLabel: {
+		fontFamily: "PoppinsBold",
+		fontSize: 16,
+	},
+	addCategoryButton: {
+		backgroundColor: PRIMARY_COLOR,
+		borderRadius: 50,
+		marginTop: 15,
+	},
+	addCategoryButtonText: {
+		fontFamily: "PoppinsBold",
+		fontSize: 16,
+		textAlign: "center",
+		paddingVertical: 15,
+		color: "white",
+	},
+	formTextInput: {
+		height: 50,
+		borderWidth: 1,
+		borderColor: "#ccc",
+		padding: 10,
+		borderRadius: 8,
+		fontFamily: "PoppinsRegular",
+	},
 	searchBar: {
 		height: 50,
 		marginBottom: 10,
@@ -181,9 +287,9 @@ const styles = StyleSheet.create({
 		position: "absolute",
 		bottom: 30,
 		right: 20,
-		backgroundColor: "#007bff",
-		width: 50,
-		height: 50,
+		backgroundColor: PRIMARY_COLOR,
+		width: 60,
+		height: 60,
 		borderRadius: 25,
 		alignItems: "center",
 		justifyContent: "center",
